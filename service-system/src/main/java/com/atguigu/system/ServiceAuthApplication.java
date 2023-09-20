89f20f131c8487fd82a96db14d3df97eeabbe8a8
Ryan <ysyxgdy@163.com>
2023-09-21 00:01:07 +0800

    push commit 1

--- service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
+++ service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
@@ -13,13 +13,6 @@ public class ServiceAuthApplication {
     public static void main(String[] args) {
         System.out.println("I love JAVA!");
         SpringApplication.run(ServiceAuthApplication.class, args);
-        //System.out.println("I also love you");
-        System.out.println("I love you too3222");
-        System.out.println("I love you three master");
-        System.out.println("greogehoewhr");
-        System.out.println("I love you three hot-fix");
-        System.out.println("3h3322342h");
-        System.out.println("greogehoewhr");
-        System.out.println("1518 EDITED");
+
     }
 }
