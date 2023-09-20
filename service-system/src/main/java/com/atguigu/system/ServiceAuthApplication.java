ffde6ab3dad9c8a2f6cf4425550d640b47652006
Ryan <ysyxgdy@163.com>
2023-09-20 13:35:35 +0800

    hot-fix first commit

--- service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
+++ service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
@@ -15,5 +15,6 @@ public class ServiceAuthApplication {
         SpringApplication.run(ServiceAuthApplication.class, args);
         System.out.println("I also love you");
         System.out.println("I love you too");
+        System.out.println("I love you three hot-fix");
     }
 }
