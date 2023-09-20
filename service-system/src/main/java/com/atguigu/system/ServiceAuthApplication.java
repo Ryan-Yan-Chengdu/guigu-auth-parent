bcb278255bb16c0f37fc4dc44901328be09b37a2
Ryan <ysyxgdy@163.com>
2023-09-20 13:26:11 +0800

    My third commit

--- service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
+++ service-system/src/main/java/com/atguigu/system/ServiceAuthApplication.java
@@ -14,5 +14,6 @@ public class ServiceAuthApplication {
         System.out.println("I love JAVA!");
         SpringApplication.run(ServiceAuthApplication.class, args);
         System.out.println("I also love you");
+        System.out.println("I love you too");
     }
 }
