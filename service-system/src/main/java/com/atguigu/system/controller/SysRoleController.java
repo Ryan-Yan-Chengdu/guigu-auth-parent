3797c9dddf9294fd39061e7e4395ec8aea4bb67f
Ryan <ysyxgdy@163.com>
2023-09-22 12:55:22 +0800

    SysRoleController添加新方法

--- service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
@@ -21,6 +21,51 @@ public class SysRoleController {
     private SysRoleService sysRoleService;
     // http://localhost:8800/admin/system/sysRole/findAll
 
+    //7.批量删除
+    @ApiOperation("批量删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> list){
+        boolean result = sysRoleService.removeByIds(list);
+        if(result){
+            return Result.ok();
+        }else{
+            return Result.fail();
+        }
+    }
+
+    //6.修改，最终修改
+    @ApiOperation("最终修改")
+    @PostMapping("update")
+    public Result updateRole(@RequestBody SysRole sysRole){
+        boolean result = sysRoleService.updateById(sysRole);
+        if(result){
+            return Result.ok();
+        }else{
+            return Result.fail();
+        }
+    }
+
+    //5.修改，根据id查询
+    @ApiOperation("根据id查询")
+    @PostMapping("findRoleById/{id}")
+    public Result findRoleById(@PathVariable Long id){
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.ok(sysRole);
+    }
+
+    //4.添加
+    @ApiOperation("添加角色")
+    @PostMapping("save")
+    public Result saveRole(@RequestBody SysRole sysRole){
+        boolean result = sysRoleService.save(sysRole);
+        if(result){
+            return Result.ok();
+        }else{
+            return Result.fail();
+        }
+    }
+
+
     //3.条件分页查询
     //page:当前页数，limit：每页的记录数
     @ApiOperation("条件分页查询")
